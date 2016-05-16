8d98fbc14a0ecfb1fc218c5e2c0c6aed0e78edf6
אליצור <@192.168.56.1>
2016-05-16 12:20:33 +0300

    הוספתי client

---
+++ OS1/src/os1/Client.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os1;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author אליצור
+ */
+public class Client {
+    Socket soc;
+    BufferedReader in;
+    PrintWriter out;
+    
+}
