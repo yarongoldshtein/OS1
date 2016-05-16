0e1913674075b01d8ca741e25fc6e853789cafb8
yaron <yaron@yaron>
2016-05-16 22:40:52 +0300

    עכשיו עם הקובץ

--- OS1/src/os1/OS1.java
+++ OS1/src/os1/OS1.java
@@ -16,6 +16,7 @@ public class OS1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hi");
     }
     
 }
---
+++ OS1/src/os1/Server.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author yaron
+ */
+public class Server implements Runnable {
+
+    private ServerSocket ServSoc;
+    static int id = 0;
+    int y;
+
+    @Override
+    public void run() {
+        String str;
+        try {
+            ServSoc = new ServerSocket(4500);
+            Socket clientSoc = ServSoc.accept();
+            PrintWriter out = new PrintWriter(clientSoc.getOutputStream());
+            InputStreamReader sr = new InputStreamReader(clientSoc.getInputStream());
+            BufferedReader in = new BufferedReader(sr);
+            while ((str = in.readLine()) != null) {
+                if (str.equals("id")) {
+                    out.println("" + id++);
+                    out.flush();
+                }
+                else{
+                   y = Integer.parseInt(str)+1;
+                   out.println(""+y);
+                   out.flush();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread s = new Thread(new Server());
+        s.start();
+
+    }
+
+}
