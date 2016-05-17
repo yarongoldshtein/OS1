3033477f800b7963c631d57a94399ea76a65051c
אליצור <@192.168.56.1>
2016-05-17 13:46:10 +0300

    הוספתי poolthread

---
+++ OS1/src/os1/ThreadPool.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author אליצור
+ */
+public class ThreadPool {
+
+    private BlockingQueue taskQueue = null;
+    private List<PoolThread> threads = new ArrayList<PoolThread>();
+    private boolean isStopped = false;
+    private final Lock lock = new ReentrantLock();
+    private Condition condVar = lock.newCondition();
+
+    public ThreadPool(int numOfThreads, int maxNumOfTasks) {
+        taskQueue = new BlockingQueue(maxNumOfTasks);
+
+        for (int i = 0; i < numOfThreads; i++) {
+            threads.add(new PoolThread(taskQueue));
+        }
+        for (PoolThread thread : threads) {
+            thread.start();
+        }
+    }
+
+    public  void execute(Runnable task) throws Exception {
+        try {
+            if (this.isStopped) {
+            throw new IllegalStateException("ThreadPool is stopped");
+        }
+
+        this.taskQueue.enqueue(task);
+        } catch (Exception e) {
+        }
+        
+    }
+
+    public void stop() {
+        try {
+            lock.lock();
+            this.isStopped = true;
+
+            for (PoolThread thread : threads) {
+                thread.doStop();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
