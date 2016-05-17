3f56c721d4d9f7a4d57c4d5117d144b70574b7eb
אליצור <@192.168.56.1>
2016-05-17 14:07:20 +0300

    bhxhui

---
+++ OS1/src/os1/BlockingQueue.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os1;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author אליצור
+ */
+public class BlockingQueue {
+
+    private List queue = new LinkedList();
+    private int limit = 10;
+    final Lock lock = new ReentrantLock();
+    private Condition cond = lock.newCondition();
+
+    public BlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public void enqueue(Object item) throws InterruptedException {
+        try {
+            lock.lock();
+            while (this.queue.size() == this.limit) {
+                cond.await();
+            }
+            if (this.queue.size() == 0) {
+                cond.signalAll();
+            }
+            this.queue.add(item);
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public  Object dequeue() throws InterruptedException {// בעיה עם ההחזרה ושחרור הנעילה
+        try {
+            lock.lock();
+             while (this.queue.size() == 0) { //cheack if queue empty
+            cond.await();
+        }
+        if (this.queue.size() == this.limit) {
+            cond.signalAll();
+        }
+        
+        } finally {
+            lock.unlock();
+        }
+        return this.queue.remove(0);
+    }
+
+}
