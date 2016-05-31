5c979b91bab2e70bc30aab2d7e4a919ac534ff09
yaron <yaron@yaron>
2016-05-31 11:29:59 +0300

    lock to cache

--- OS1/src/os1/WriteDataBase.java
+++ OS1/src/os1/WriteDataBase.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package os1;
 
 import java.io.File;
@@ -65,7 +60,6 @@ public class WriteDataBase implements Runnable {
                             }
                         }
                     }
-
                     Server.waitersToWriteInDb.clear();
 
                 }
